f69ae6582e6b8f0b7c696bdd5d4965861552acc1
sadmiral <rizkysptr3798@gmail.com>
2024-11-15 16:19:10 +0700

    change inherit

--- controller/main.py
+++ controller/main.py
@@ -8,14 +8,20 @@ import requests
 from bs4 import BeautifulSoup
 import logging
 _logger = logging.getLogger(__name__)
+from odoo.addons.whatsapp.controller.main import Webhook
 
-class WebController(http.Controller):
-    @http.route('/whatsapp/webhook/', methods=['POST'], type="json", auth="public")
+class WebController(Webhook):
+    # @http.route('/testwebhookpost', methods=['POST'], type="json", auth="public")
+    # def testwebhookpost(self):
+    #     data = json.loads(request.httprequest.data)
+    #     print("\n\n data webhook :")
+    #     print(data)
+
+    @http.route()
     def webhookpost(self):
         data = json.loads(request.httprequest.data)
         
-        # Custom behavior: print the data
-        _logger.info("Webhook Data: %s", json.dumps(data, indent=4))
+        _logger.info("\n\nWebhook Data: %s", json.dumps(data, indent=4))
         
         if request.env.company.ngrok_url:
             _logger.info("\n\nPOST : ngrok url found!")
@@ -23,20 +29,24 @@ class WebController(http.Controller):
             response = requests.post(url, json=data)
         else:
             _logger.info("\n\nPOST : ngrok url not found!")
+        super().webhookpost()
+    # @http.route('/whatsapp/webhook/', methods=['POST'], type="json", auth="public")
+    # def webhookpost(self):
+        
 
-        # Call the original method to ensure parent logic is executed
-        super(WebController, self).webhookpost()
+    #     # Call the original method to ensure parent logic is executed
+    #     super(WebController, self).webhookpost()
 
     
-    @http.route('/whatsapp/webhook/', methods=['GET'], type="http", auth="public", csrf=False)
-    def webhookget(self, **kwargs):
-        _logger.info("Received GET webhook request with data: %s", kwargs)
+    # @http.route('/whatsapp/webhook/', methods=['GET'], type="http", auth="public", csrf=False)
+    # def webhookget(self, **kwargs):
+    #     _logger.info("Received GET webhook request with data: %s", kwargs)
 
-        if request.env.company.ngrok_url:
-            _logger.info("\n\n GET : ngrok url found!")
-            url = request.env.company.ngrok_url
-            response = requests.get(url, params=kwargs)
-        else:
-            _logger.info("\n\n GET : ngrok url not found!")
+    #     if request.env.company.ngrok_url:
+    #         _logger.info("\n\n GET : ngrok url found!")
+    #         url = request.env.company.ngrok_url
+    #         response = requests.get(url, params=kwargs)
+    #     else:
+    #         _logger.info("\n\n GET : ngrok url not found!")
         
-        super(WebController, self).webhookget(**kwargs)
+    #     super(WebController, self).webhookget(**kwargs)
