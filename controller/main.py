13168dae74669f457c065df53db210605e6b80a8
sadmiral <rizkysptr3798@gmail.com>
2024-11-18 12:02:10 +0700

    trying button reply

--- controller/main.py
+++ controller/main.py
@@ -11,23 +11,94 @@ _logger = logging.getLogger('dke.iziapp.id')
 from odoo.addons.whatsapp.controller.main import Webhook
 from odoo import _
 from odoo.addons.whatsapp.tools.whatsapp_api import WhatsAppApi
+from odoo.addons.whatsapp.tools.whatsapp_exception import WhatsAppError
 
 # R: monkey patching
 original_send_whatsapp = WhatsAppApi._send_whatsapp
 def custom_send_whatsapp(self, number, message_type, send_vals, parent_message_id=False):
+    # data = {
+    #     'number':number,
+    #     'message_type':message_type,
+    #     'send_vals':send_vals,
+    #     'parent_message_id':parent_message_id
+    # }
+    # if request.env.company.ngrok_url:
+    #     _logger.info("\n\ndke.iziapp.id : POST Send Whatsapp : ngrok url found!")
+    #     url = f"{request.env.company.ngrok_url}testsendwhatsapp"
+    #     response = requests.post(url, json=data)
+    # else:
+    #     _logger.info("\n\ndke.iziapp.id : POST Send Whatsapp : ngrok url NOT found!")
+    # return original_send_whatsapp(self, number, message_type, send_vals, parent_message_id)
+    """ Send WA messages for all message type using WhatsApp Business Account
+
+    API Documentation:
+        Normal        - https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages
+        Template send - https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-message-templates
+    """
     data = {
-        'number':number,
-        'message_type':message_type,
-        'send_vals':send_vals,
-        'parent_message_id':parent_message_id
+        'messaging_product': 'whatsapp',
+        'recipient_type': 'individual',
+        'to': number
     }
-    if request.env.company.ngrok_url:
-        _logger.info("\n\ndke.iziapp.id : POST Send Whatsapp : ngrok url found!")
-        url = f"{request.env.company.ngrok_url}testsendwhatsapp"
-        response = requests.post(url, json=data)
-    else:
-        _logger.info("\n\ndke.iziapp.id : POST Send Whatsapp : ngrok url NOT found!")
-    return original_send_whatsapp(self, number, message_type, send_vals, parent_message_id)
+    # if there is parent_message_id then we send message as reply
+    if parent_message_id:
+        data.update({
+            'context': {
+                'message_id': parent_message_id
+            },
+        })
+    if message_type in ('template','document', 'image', 'audio', 'video'):
+        data.update({
+            'type': message_type,
+            message_type: send_vals
+        })
+    if message_type == 'text':
+        data.update({
+            'type': 'interactive',
+            'interactive': {
+                'type': 'button',
+                'header': {
+                    'type':'text',
+                    'text': 'Testing Reply Button'
+                },
+                'body': {
+                    'text': send_vals.get('body')
+                },
+                'action': {
+                    'buttons': [
+                        {
+                        'type': 'reply',
+                        'reply': {
+                            'id': 'reply-yes',
+                            'title': 'Yeah !'
+                            }
+                        },
+                        {
+                        'type': 'reply',
+                        'reply': {
+                            'id': 'reply-no',
+                            'title': 'Nope ?!'
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+
+    json_data = json.dumps(data)
+    _logger.info("Send %s message from account %s [%s]", message_type, self.wa_account_id.name, self.wa_account_id.id)
+    response = self.__api_requests(
+        "POST",
+        f"/{self.phone_uid}/messages",
+        auth_type="bearer",
+        headers={'Content-Type': 'application/json'},
+        data=json_data
+    )
+    response_json = response.json()
+    if response_json.get('messages'):
+        msg_uid = response_json['messages'][0]['id']
+        return msg_uid
+    raise WhatsAppError(*self._prepare_error_response(response_json))
 
 WhatsAppApi._send_whatsapp = custom_send_whatsapp
 class WebController(Webhook):
