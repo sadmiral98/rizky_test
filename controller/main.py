643d63a34868ecf079146209e3bfc853373367b8
sadmiral <rizkysptr3798@gmail.com>
2024-11-18 13:15:09 +0700

    -

--- controller/main.py
+++ controller/main.py
@@ -86,10 +86,10 @@ def custom_send_whatsapp(self, number, message_type, send_vals, parent_message_i
         })
 
     json_data = json.dumps(data)
-    _logger.info("Send %s message from account %s [%s]", message_type, WhatsAppApi.wa_account_id.name, WhatsAppApi.wa_account_id.id)
+    _logger.info("Send %s message from account %s [%s]", message_type, self.wa_account_id.name, self.wa_account_id.id)
     response = WhatsAppApi.__api_requests(
         "POST",
-        f"/{WhatsAppApi.phone_uid}/messages",
+        f"/{self.phone_uid}/messages",
         auth_type="bearer",
         headers={'Content-Type': 'application/json'},
         data=json_data
