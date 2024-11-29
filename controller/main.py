be6f65a1fd16eda3fc370e73aa789c8d3132d209
sadmiral <rizkysptr3798@gmail.com>
2024-11-29 16:36:24 +0700

    manual test

--- controller/main.py
+++ controller/main.py
@@ -103,7 +103,7 @@ def custom_process_document(self, data, send_vals, discuss_data):
     try:
         attachment_id = discuss_data.get('discuss_attachment') #this returning 1371. and id 1371 is do exist!
         _logger.info("1") #but logging is stopping on here! logger "2" doesnt printed?? why?
-        attachment = request.env['ir.attachment'].sudo().search([('id','=',int(attachment_id))]) 
+        attachment = request.env['ir.attachment'].sudo().browse(1371)
         _logger.info("2")
         _logger.info(attachment)
         file_content = base64.b64decode(attachment.datas)
